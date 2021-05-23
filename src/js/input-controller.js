a16ea14ef83572083997e33652f81f1e472e66ff
SeerLite <seerlite@tuta.io>
2021-05-23 16:20:53 -0400

    Don't backspace when still in compose mode

--- src/js/input-controller.js
+++ src/js/input-controller.js
@@ -739,7 +739,7 @@ $("#wordsInput").on("input", function (event) {
       event.target.value = " ";
       backspaceToPrevious();
       Replay.addReplayEvent("backWord");
-    } else {
+    } else if (!Misc.trailingComposeChars.test(inputValue)) {
       TestUI.updateWordElement();
       // TODO: this is broken
       for (
