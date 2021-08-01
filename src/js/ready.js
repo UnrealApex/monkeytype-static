3e269214c766e0de3cca6ba482f6a8d9164b00a3
SeerLite <seerlite@tuta.io>
2021-08-01 14:43:56 -0400

    Update ready.js

--- src/js/ready.js
+++ src/js/ready.js
@@ -5,7 +5,7 @@ import * as Settings from "./settings";
 import * as RouteController from "./route-controller";
 import * as UI from "./ui";
 import * as MonkeyPower from "./monkey-power";
-
+console.log("right");
 ManualRestart.set();
 Misc.migrateFromCookies();
 UpdateConfig.loadFromLocalStorage();
